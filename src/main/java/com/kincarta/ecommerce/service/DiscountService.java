446bd57df4f8f30e1cd77066796b4b0bef7ea302
Vinnie <vinicius.ramaciotti.garcia@ericsson.com>
2020-12-08 19:49:13 +0000

    Change Discount Service

--- src/main/java/com/kincarta/ecommerce/service/DiscountService.java
+++ src/main/java/com/kincarta/ecommerce/service/DiscountService.java
@@ -20,40 +20,31 @@ public class DiscountService {
 
     //Posso mudar esse metodo para uma classe especifica que toma conta desse "match" entre produtos e descontos
     public Double scan(String[] items){
-        List<String> list = Arrays.asList(items);
-
-        ArrayList<String> listTwo = new ArrayList(list);
-
-        HashSet hs = new HashSet<>();
-        hs.addAll(listTwo);
-        listTwo.clear();
-        listTwo.addAll(hs);
-
-        ArrayList<String> results = new ArrayList();
-        for(String word : listTwo){
-            int count = Collections.frequency(list, word);
-            String result = word + ":" + count;
-            results.add(result);
-        }
-
         List<Item> allItems = new ArrayList(loader.getAllItems());
         List<PromotionalRules> discountItems = new ArrayList(loader.getDiscountItems());
         Double totalPrice = 0.0;
 
-        for (String notRepeatedProduct : results) {
+        ArrayList<String> duplications = getDuplications(items);
+
+        for (String notRepeatedProduct : duplications) {
             String[] element = notRepeatedProduct.split(":");
-            //Posso melhorar pra pegar a quantidate (no caso 2) depois que achar ele na lista de promotionalRules
-            if (Integer.valueOf(element[1]) >= 2){
-                Optional<PromotionalRules> productPromotionalRuleItem = discountItems.stream().filter(p -> element[0].equals(p.getProductId())).findFirst();
-                if (productPromotionalRuleItem.isPresent()){
-                    totalPrice += Integer.valueOf(element[1]) * productPromotionalRuleItem.get().getPrice();
+            //element[0] -> product number
+            //element[1] -> quantity
+            Optional<PromotionalRules> productPromotionalRuleItem = discountItems.stream().filter(p -> element[0].equals(p.getProductId())).findFirst();
+            if(productPromotionalRuleItem.isPresent()){
+                //Posso melhorar pra pegar a quantidate (no caso 2) depois que achar ele na lista de promotionalRules
+                if (Integer.valueOf(element[1]) >= productPromotionalRuleItem.get().getQty()){
+                    productPromotionalRuleItem = discountItems.stream().filter(p -> element[0].equals(p.getProductId())).findFirst();
+                    if (productPromotionalRuleItem.isPresent()){
+                        totalPrice += Integer.valueOf(element[1]) * productPromotionalRuleItem.get().getPrice();
+                    }else{
+                        Optional<Item> productItem = allItems.stream().filter(p -> element[0].equals(p.getId())).findFirst();
+                        totalPrice += Integer.valueOf(element[1]) * productItem.get().getPrice();
+                    }
                 }else{
                     Optional<Item> productItem = allItems.stream().filter(p -> element[0].equals(p.getId())).findFirst();
                     totalPrice += Integer.valueOf(element[1]) * productItem.get().getPrice();
                 }
-            }else{
-                Optional<Item> productItem = allItems.stream().filter(p -> element[0].equals(p.getId())).findFirst();
-                totalPrice += Integer.valueOf(element[1]) * productItem.get().getPrice();
             }
         }
 
@@ -63,5 +54,23 @@ public class DiscountService {
         }
 
         return Math.floor(totalPrice * 100) / 100;
+    }
+
+    private ArrayList<String> getDuplications(String[] items) {
+        ArrayList<String> duplications = new ArrayList();
+        List<String> list = Arrays.asList(items);
+        ArrayList<String> listTwo = new ArrayList(list);
+
+        HashSet hs = new HashSet<>();
+        hs.addAll(listTwo);
+        listTwo.clear();
+        listTwo.addAll(hs);
+
+        for(String word : listTwo){
+            int count = Collections.frequency(list, word);
+            String result = word + ":" + count;
+            duplications.add(result);
+        }
+        return duplications;
     }
 }
